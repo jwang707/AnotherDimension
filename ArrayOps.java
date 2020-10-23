fdac3108fd0b20f198d7eb597aeb345e289f6301
Jasmine <jwang2367@gmail.com>
2020-10-23 13:15:31 -0400

    largest() works now, and I added an extra tester statement to account for sum()'s input being an empty array

--- ArrayOps.java
+++ ArrayOps.java
@@ -4,21 +4,26 @@ public class ArrayOps {
     for (int i = 0; i < arr.length; i++) {
       count += arr[i];
     }
-
     return count;
   }
 
+  public static int largest(int[] arr) {
+    int result = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > result) {
+        result = arr[i];
+      }
+    }
+    return result;
+  }
 
   public static void main(String[] args) {
     int[] myNums = {0, 20, 3 , 1, 2};
+    int[] blank = {};
 
     System.out.println(sum(myNums));
+    System.out.println(sum(blank));
+    System.out.println(largest(myNums));
   }
-
-
-
-
-
-
 
 }
