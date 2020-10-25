28ffb24508f11728c2763d2fcda9f9528ebb87db
Jasmine <jwang2367@gmail.com>
2020-10-25 17:26:09 -0400

    everything works, debugged sum a little bit, shoutout to maxwell zen for letting me use his test cases

--- ArrayOps.java
+++ ArrayOps.java
@@ -37,7 +37,7 @@ public class ArrayOps {
   public static int sum(int[][] matrix) {
     int result = 0;
     for (int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j <= matrix.length; j++)
+      for (int j = 0; j < matrix[i].length; j++)
         result += matrix[i][j];
   }
     return result;
